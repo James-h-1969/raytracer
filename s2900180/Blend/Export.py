fc404861a39398b69674893f1a2ce1d73696b762
James-h-1969 <jameshocking542@gmail.com>
2025-10-06 13:28:56 +0100

    adding json dump and object type to exporter

--- s2900180/Blend/Export.py
+++ s2900180/Blend/Export.py
@@ -8,6 +8,10 @@
 import bpy 
 import json 
 
+### PARAMETERS: Only change here 
+OUTPUT_FILE_PATH="output.json"
+################################
+
 def get_blend_object_as_dict() -> dict:
     """
     This function uses the bpy library to process the objects 
@@ -21,6 +25,28 @@ def get_blend_object_as_dict() -> dict:
 
     for object in bpy.data.objects:
         if not object.hide_get(): # only export visible objects
+            object_type: str = object.type
+            current_object = {
+                "type":object_type
+            }
+
+            if object_type == "camera":
+                current_object["location"] = ...
+                current_object["gaze_vector_direction"] = ...
+                current_object["focal_length"] = ...
+                current_object["sensor_width"] = ...
+                current_object["sensor_height"] = ...
+                current_object["film_resolution"] = ...
+            if object_type == "light":
+                ...
+
+            blend_object["objects"].append(current_object)
+    
+    return blend_object
+
 
 if __name__ == "__main__":
-    blend_to_json()
+    blend_object = get_blend_object_as_dict()
+
+    with open(OUTPUT_FILE_PATH, "w") as file:
+        json.dump(blend_object, file, indent=4)
